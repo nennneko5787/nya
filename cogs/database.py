fe6c09438b44d250b666cf5b4a8b5736437afebd
ねんねこ <nennneko5787@gmail.com>
2025-02-20 17:24:44 +0900

    database.py を更新

--- cogs/database.py
+++ cogs/database.py
@@ -22,58 +22,58 @@ class Database:
         cls.pool = await asyncpg.create_pool(os.getenv("dsn"), statement_cache_size=0)
         with open("./credentials.json") as f:
             data: Dict[str, Dict[str, str]] = json.load(f)
-        paypay = PayPay()
+        cls.paypay = PayPay()
         if not data["paypay"]["accessToken"]:
             if not data["paypay"]["clientUUID"]:
-                await paypay.initialize(
+                await cls.paypay.initialize(
                     data["paypay"]["phone"],
                     data["paypay"]["password"],
                 )
+                await cls.paypay.login(input("OTP: "))
             else:
-                await paypay.initialize(
+                await cls.paypay.initialize(
                     data["paypay"]["phone"],
                     data["paypay"]["password"],
                     data["paypay"]["deviceUUID"],
                     data["paypay"]["clientUUID"],
                 )
-            await paypay.login(input("OTP: "))
         else:
-            await paypay.initialize(access_token=data["paypay"]["accessToken"])
+            await cls.paypay.initialize(access_token=data["paypay"]["accessToken"])
             try:
-                await paypay.get_balance()
+                await cls.paypay.get_balance()
             except:
-                await paypay.token_refresh(data["paypay"]["refreshToken"])
+                await cls.paypay.token_refresh(data["paypay"]["refreshToken"])
 
-        data["paypay"]["deviceUUID"] = paypay.device_uuid
-        data["paypay"]["clientUUID"] = paypay.client_uuid
-        data["paypay"]["accessToken"] = paypay.access_token
+        data["paypay"]["deviceUUID"] = cls.paypay.device_uuid
+        data["paypay"]["clientUUID"] = cls.paypay.client_uuid
+        data["paypay"]["accessToken"] = cls.paypay.access_token
         try:
-            data["paypay"]["refreshToken"] = paypay.refresh_token
+            data["paypay"]["refreshToken"] = cls.paypay.refresh_token
         except:
             pass
 
         with open("./credentials.json", "r+") as f:
             json.dump(data, f)
 
-        kyash = Kyash()
+        cls.kyash = Kyash()
         if not data["kyash"]["clientUUID"]:
-            await kyash.login(
+            await cls.kyash.login(
                 data["kyash"]["email"],
                 data["kyash"]["password"],
             )
         else:
-            await kyash.login(
+            await cls.kyash.login(
                 data["kyash"]["email"],
                 data["kyash"]["password"],
                 data["kyash"]["clientUUID"],
                 data["kyash"]["installationUUID"],
             )
 
-        if not kyash.access_token:
-            await kyash.validate_otp(input("OTP: "))
+        if not cls.kyash.access_token:
+            await cls.kyash.validate_otp(input("OTP: "))
 
-        data["kyash"]["installationUUID"] = kyash.installation_uuid
-        data["kyash"]["clientUUID"] = kyash.client_uuid
+        data["kyash"]["installationUUID"] = cls.kyash.installation_uuid
+        data["kyash"]["clientUUID"] = cls.kyash.client_uuid
 
         with open("./credentials.json", "r+") as f:
             json.dump(data, f)
